4c2ad57a277d99fce42a86696c179811d45e25f9
manujacobt <manujacobt@gmail.com>
2021-09-12 20:58:48 +0530

    2nd

--- js/grocery.js
+++ js/grocery.js
@@ -7,20 +7,34 @@ $("button").click(function click(){
         var grocerydata = '';
         $.each(data, function(key, value){
             grocerydata += `<tr>`;
-              grocerydata += `<td>`+value.sno+`</td>`;
-              grocerydata += `<td>`+value.name+`</td>`;
-              grocerydata += `<td>`+value.qty+`</td>`;
-              grocerydata += `<td>`+value.unit+`</td>`;
-              grocerydata += `<td>`+value.dpt+`</td>`;
-              grocerydata += `<td>`+value.notes+`</td>`;
+              grocerydata += `<td id="td">`+value.sno+`</td>`;
+              grocerydata += `<td id="td">`+value.name+`</td>`;
+              grocerydata += `<td id="td">`+value.qty+`</td>`;
+              grocerydata += `<td id="td">`+value.unit+`</td>`;
+              grocerydata += `<td id="td">`+value.dpt+`</td>`;
+              grocerydata += `<td id="td">`+value.notes+`</td>`;
             grocerydata += `</tr>`;
         });
         $('#grocery-list').append(grocerydata); 
         var row= document.getElementsByTagName("tr");
+        var cl= document.getElementsByTagName("td");
         row[0].style.color="black";
         row[0].style.fontSize="large";
         row[0].style.fontWeight="bold";
         row[0].style.background="green";
+        for(var i=0;i<row.length;i++){
+          row[i].style.height="-10px";
+          row[i].style.textAlign="center";
+         
+          
+        }
+          for(var j=6;j<cl.length;j++){
+          cl[j].style.border="solid red";
+                 
+          
+         }
+         
+        
     });
     });
     $("button").attr("disabled",true);
